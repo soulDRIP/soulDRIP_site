5ed5564cc48ee3eeb4a0e79fbc09672d7f444c33
mihae <munteanmihael@outlook.com>
2023-02-21 11:51:37 +0200

    Add files

---
+++ resources/HTML/app.js
@@ -0,0 +1,16 @@
+const shareButtons = document.querySelectorAll('.tile-share-button','share-button');
+async function copyText(e) {
+ e.preventDefault();
+ const link = this.getAttribute('link')
+ try{
+    await navigator.clipboard.writeText('link')
+    alert("Copied to clipboard" +' '+ link)
+    }catch (err) {
+        console.log(err)
+    }
+};
+
+shareButtons.forEach(shareButton => 
+    shareButton.addEventListener('click',copyText));
+
+
