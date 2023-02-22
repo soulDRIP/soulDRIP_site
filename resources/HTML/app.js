895b1698b54bd7dd265e66360d33e12888a96cc5
Mihai <munteanmihael@outlook.com>
2023-02-22 17:55:58 +0200

    duda reparatie

--- resources/HTML/app.js
+++ resources/HTML/app.js
@@ -15,26 +15,7 @@ shareButtons.forEach(shareButton =>
 
 
 const dropdownButton = document.getElementById('dropdown-button');
-const dropdownMenu = document.getElementById('dropdown-menu');
 const contents = document.getElementById('dropmenu-contents');
-const contactContents=document.getElementById('contact-contents');
 dropdownButton.addEventListener('click', () => {
-  if (!contents.classList.contains('show')) {
-    contents.innerHTML =contactContents.innerHTML;
-    contents.style.height = '100px';
-  } else {
-    contents.style.height = '0';
-    contents.addEventListener('transitionend', transitionEndHandler);
-  }
-  contents.classList.toggle('show');
+    contents.classList.toggle('active');
 });
-
-function transitionEndHandler() {
-  contents.innerHTML = '';
-  contents.classList.remove('show');
-  contents.removeEventListener('transitionend', transitionEndHandler);
-}
-    //<a href="https://www.instagram.com/mihai_souldrip_/">Instagram</a>
-      // <a href="https://www.facebook.com/mi.soulDRIP/">Facebook</a>"
-   
-   
